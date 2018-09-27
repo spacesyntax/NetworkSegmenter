ddf687740b1dd9d86b7b36e2831cfbbb258d680a
joe <i.kolovou@spacesyntax.com>
2018-09-27 08:48:03 +0100

    to test - stubs iterator

--- test_script.py
+++ test_script.py
@@ -13,13 +13,15 @@ buffer = 0
 errors = True
 # my_segmentor = segmentor(layer, None, stub_ratio, None)
 my_segmentor = segmentor(layer, unlinks, stub_ratio, buffer, errors)
+my_segmentor.load_graph()
 
+start_time = time.time()
 break_lines, break_points = my_segmentor.segment()
 print 'process time', time.time() - start_time
 
-segmented = to_layer(break_lines, layer.crs(), layer.dataProvider().encoding(), layer.dataProvider().geometryType(), "shapefile", '/Users/joe/segmented.shp', 'segmented')
+segmented = to_layer(break_lines, layer.crs(), layer.dataProvider().encoding(), layer.dataProvider().geometryType(), "shapefile", '/Users/joe/merged_mixed_model1_seg.shp', 'merged_mixed_model1_seg')
 QgsMapLayerRegistry.instance().addMapLayer(segmented)
-errors = to_layer(break_points, layer.crs(), layer.dataProvider().encoding(), 1, "shapefile", '/Users/joe/errors.shp', 'segmented points')
+errors = to_layer(break_points, layer.crs(), layer.dataProvider().encoding(), 1, "shapefile", '/Users/joe/merged_mixed_model1_seg_errors.shp', 'merged_mixed_model1_seg_errors')
 QgsMapLayerRegistry.instance().addMapLayer(errors)
 
 print 'process time', time.time() - start_time
